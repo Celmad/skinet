3c89b350b509ff33f57aad114dae677983fcb2cb
Manuel Alaminos Dominguez <mad@iMac-de-Manuel.local>
2022-08-27 12:21:26 +0200

    Updating project to pick it up after a while

--- Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
+++ Infrastructure/Data/Migrations/StoreContextModelSnapshot.cs
@@ -12,7 +12,7 @@ namespace Infrastructure.Data.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "6.0.2");
+            modelBuilder.HasAnnotation("ProductVersion", "6.0.8");
 
             modelBuilder.Entity("Core.Entities.Product", b =>
                 {
