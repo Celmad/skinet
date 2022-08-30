0b0327a0df0e10be3eca22433568eac6f690e034
Manuel Alaminos Dominguez <manuelalaminosd@pm.me>
2022-08-30 18:37:12 +0200

    2 new API methods added for product brands and product types

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -15,17 +15,28 @@ namespace API.Controllers
             _repo = repo;
         }
 
-        [HttpGet]
-        public ActionResult<List<Product>> GetProducts()
-        {
-            var products = _repo.GetProductsAsync();
-            return Ok(products);
-        }
-
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
             return await _repo.GetProductByIdAsync(id);
-        }   
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+        {
+            return Ok(await _repo.GetProductsAsync());
+        }
+
+        [HttpGet("types")]
+        public async Task<ActionResult<IReadOnlyList<ProductType>>> GetProductTypes()
+        {
+            return Ok(await _repo.GetProductTypesAsync());
+        }
+
+        [HttpGet("brands")]
+        public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetProductBrands()
+        {
+            return Ok(await _repo.GetProductBrandsAsync());
+        }
     }
 }
