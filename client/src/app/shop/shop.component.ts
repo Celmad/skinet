127d91e109cb850f8a4555c795c7a990566ed0bf
Manuel Alaminos Dominguez <manuelalaminosd@protonmail.com>
2022-12-04 22:31:32 +0100

    Product item component and more dynamic data added

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,14 +11,36 @@ import { ShopService } from './shop.service';
 })
 export class ShopComponent implements OnInit {
   products: IProduct[];
+  types: IType[];
+  brands: IBrand[];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(): void {
+    this.getProducts();
+    this.getTypes();
+    this.getBrands();
+  }
+
+  getProducts(): void {
     this.shopService.getProducts().subscribe({
       next: (v) => this.products = v.data,
       error: (e) => console.error(e)
     });
+  }
+
+  getTypes(): void {
+    this.shopService.getTypes().subscribe({
+      next: (v) => this.types = v,
+      error: (e) => console.error(e)
+    })
+  }
+
+  getBrands(): void {
+    this.shopService.getBrands().subscribe({
+      next: (v) => this.brands = v,
+      error: (e) => console.error(e)
+    })
   }
 
 }
--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FontAwesomeModule
   ],
   exports: [ShopComponent]
 })
